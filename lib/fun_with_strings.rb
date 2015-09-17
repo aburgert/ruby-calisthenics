795834546b629a11ad43b87b82c4b79e5cafcf68
aburgert <aburgert@uncc.edu>
2015-09-17 19:14:57 +0000

    changes after lab 09/17; completed fws 1-3 and enabled tests fws 1-4

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,9 +1,27 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    #1. strip non-alpha characters w/ gsub?
+    pal = self
+    pal.downcase!
+    #puts "Pre-substitution\t#{pal}"
+    pal.gsub!(/[^a-z]/, '')
+    #puts "Post-substitution:\t#{pal}"
+    #2. separate string into two parts - odd-lengths have middle in both strings
+    #2a. self.reverse
+    lap = pal.reverse
+    return pal == lap ? true : false
   end
   def count_words
-    # your code here
+      arr = self.downcase.gsub(/[^a-z ]/, '').split(' ')
+      ht = Hash.new(0)
+      arr.each do |w|
+        ht[w] += 1
+      end
+      
+      #ht.each do |key, val|
+      #  puts "#{key}\t==>\t#{val}"
+      #end
+      return ht
   end
   def anagram_groups
     # your code here
