cf1093c2f8843d14049a87e5e217cfdacbd61f8d
aburgert <aburgert@uncc.edu>
2015-10-05 16:44:37 +0000

    Got attr_accessor_with_history working by appending to _history array

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -5,15 +5,17 @@ class Class
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q"
       def #{attr_name}=(input)
-        puts \"test\"
+        #puts \"test\"
+        @#{attr_name}_history ||= Array.new()
+        @#{attr_name}_history << #{attr_name}
         @#{attr_name} = input
-        puts @#{attr_name}
+        #puts @#{attr_name}
         
       end"
   end
 end
 
-class TestClass1
+class TesterClass
   attr_accessor_with_history :testvar
   #attr_accessor :testvar
   def initialize(testvar)
@@ -21,8 +23,10 @@ class TestClass1
   end
 end
 
-tc = TestClass1.new(0)
-puts tc.testvar.to_s #=> "0"
-tc.testvar = "testing"
-puts "#{tc.testvar} 2" # => "test 2"
-puts tc.testvar_history # => arrayish
+#tc = TesterClass.new(0)
+#puts tc.testvar.to_s #=> "0"
+#tc.testvar = "testing"
+#puts "#{tc.testvar} 2" # => "testing 2"
+#puts tc.testvar_history # => arrayish
+#tc.testvar = "testing some more"
+#puts tc.testvar_history
